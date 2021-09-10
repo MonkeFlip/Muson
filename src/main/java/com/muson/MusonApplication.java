bea4cc2a6897be287b27b6acd9ba110b9d0de0a8
artiom.labietskii <artiom.labietskii@gmail.com>
2021-09-11 00:18:41 +0300

    Initial commit

---
+++ src/main/java/com/muson/MusonApplication.java
@@ -0,0 +1,13 @@
+package com.muson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muson/MusonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
