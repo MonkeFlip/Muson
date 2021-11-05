a0dbd76ccc28bf9b49ab5d715d2aa864acd4fe54
BoeseKnight <ilia.lamashka@gmail.com>
2021-11-05 21:48:57 +0300

    i did not change anything but i can not pull your repository, so please merge me...

--- src/main/java/com/muson/MusonApplication.java
+++ src/main/java/com/muson/MusonApplication.java
@@ -22,23 +22,4 @@ public class MusonApplication {
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        final
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*"); // this allows all origin
-        config.addAllowedHeader("*"); // this allows all headers
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("HEAD");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("DELETE");
-        config.addAllowedMethod("PATCH");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
 }
