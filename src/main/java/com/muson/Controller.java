6ea98653aa894580a712fd8ca55305d09c4da65a
BoeseKnight <ilia.lamashka@gmail.com>
2021-11-07 06:49:36 +0300

    streaming is working

--- src/main/java/com/muson/Controller.java
+++ src/main/java/com/muson/Controller.java
@@ -4,6 +4,7 @@ import com.muson.SongsAndGenres.GenreDAO;
 import com.muson.SongsAndGenres.Song;
 import com.muson.SongsAndGenres.SongRepo;
 import lombok.RequiredArgsConstructor;
+import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.*;
@@ -55,23 +56,28 @@ public class Controller {
         return songRepo.findAllByArtist(artist);
     }
 
+    @GetMapping("/findByGenre")
+    public List<Song> showByGenre(@RequestParam(value = "genre", defaultValue = "default song") String genre)
+    {
+        return songRepo.findAllByGenre(genre);
+    }
+
     @GetMapping(value="/stream/{songName}", produces = {
             MediaType.APPLICATION_OCTET_STREAM_VALUE })
+    @CrossOrigin()
     public ResponseEntity playAudio(HttpServletRequest request, HttpServletResponse response, @PathVariable("songName") String songName) throws FileNotFoundException {
 
 //        System.out.println("[downloadRecipientFile]");
-
-        String file = "D:\\Music\\" + songName;
-
+        var songVar=songRepo.findById(Integer.valueOf(songName));
+        Song song = songVar.get();
+        String file = song.getDirectory()+'/'+song.getSong();
+//        file = file.replaceAll("%20", " ");
         long length = new File(file).length();
-
-
+        System.out.println("stream ok"+ file);
         InputStreamResource inputStreamResource = new InputStreamResource( new FileInputStream(file));
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentLength(length);
         httpHeaders.setCacheControl(CacheControl.noCache().getHeaderValue());
         return new ResponseEntity(inputStreamResource, httpHeaders, HttpStatus.OK);
     }
-
-
 }
